de0df536aa93919d3b6c96ed4cd69f3b7c271ee4
aishive <haishive@hotmail.com>
2022-05-11 21:39:17 -0700

    finish save

--- index.js
+++ index.js
@@ -6,8 +6,46 @@ object.style.left = left + 'px'
 object.style.bottom = bottom + 'px'
 document.body.append(object)
 }
+
+
+function newItem (url, left, bottom) {
+    let object = document.createElement('img')
+object.src = url
+object.style.position = 'fixed'
+object.style.left = left + 'px'
+object.style.bottom = bottom + 'px'
+document.body.append(object)
+
+object.addEventListener('dblclick', () => {
+    object.remove()
+})
+}
+
 newImage('assets/green-character.gif', '100', '100')
 newImage('assets/pine-tree.png', '450', '200')
+newImage('assets/tree.png', '200', '300')
+newImage('assets/pillar.png', '350', '100')
+newImage('assets/crate.png', '150', '200')
+newImage('assets/well.png', '500', '425')
+
+newItem('assets/sword.png', '500', '405')
+newItem('assets/sheild.png', '165', '185')
+newItem('assets/staff.png', '600', '100')
+
+
+/*assets/shield.png (165px from the left, 185px from the bottom)
+assets/staff.png (600px from the left, 100px from the bottom)
+
+/*let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
 
 
 
@@ -21,10 +59,3 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)*/
-
-/*let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)*/
